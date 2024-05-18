957bcf56edbdfe30bf10aa6a58520ec4b354a8f2
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-05-18 13:50:15 -0600

    Eliminando los using

--- Logica_Negocio/RolBL.cs
+++ Logica_Negocio/RolBL.cs
@@ -1,10 +1,6 @@
 ﻿using Acceso_Datos;
 using Entidades;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Logica_Negocio
 {
